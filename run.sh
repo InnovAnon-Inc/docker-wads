111a30b304da46bb94bc2df51e43b0c8e37fdf05
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-24 00:04:11 -0500

    removed data

--- run.sh
+++ run.sh
@@ -16,9 +16,9 @@ dockerd &
 sudo             -- \
 nice -n +20      -- \
 sudo -u `whoami` -- \
-docker build -t innovanon/wads .
+docker build -t innovanon/docker-wads .
 
-docker push innovanon/wads:latest || :
+docker push innovanon/docker-wads:latest || :
 
 docker volume inspect wadsvol ||
 docker volume create  wadsvol
@@ -27,20 +27,22 @@ xhost +local:`whoami`
 sudo             -- \
 nice -n +20      -- \
 sudo -u `whoami` -- \
-docker run   -t --net=host -e DISPLAY=${DISPLAY} --mount source=wadsvol,target=/usr/out --rm --name wads innovanon/wads
-#docker run   -t --mount source=wadsvol,target=/root/oblige --rm --name wads innovanon/wads
+docker run   -t --net=host -e DISPLAY=${DISPLAY} --mount source=wadsvol,target=/usr/vol --rm --name docker-wads innovanon/docker-wads
+#docker run   -t --net=host -e DISPLAY=${DISPLAY} --rm --name docker-wads innovanon/docker-wads
+#docker run   -t --mount source=wadsvol,target=/usr/vol --rm --name docker-wads innovanon/docker-wads
 
 # https://www.reddit.com/r/docker/comments/9ou9wx/getting_build_artifacts_out_of_docker_image/
 
 # Create but don't run container from resulting image
-CID=$(docker create --mount source=wadsvol,target=/root/oblige innovanon/wads)
+CID=$(docker create --mount source=wadsvol,target=/usr/vol innovanon/docker-wads)
 
 # Container be gone
 trap "docker rm ${CID}" 0
 
 # Grab that artifact sweetness
-docker cp ${CID}:/usr/out/Project_Brutality.pk3 .
-docker cp ${CID}:/usr/out/freedm.wad            .
-docker cp ${CID}:/usr/out/freedoom1.wad         .
-docker cp ${CID}:/usr/out/freedoom2.wad         .
+docker cp ${CID}:/usr/vol/Project_Brutality.pk3 .
+docker cp ${CID}:/usr/vol/rainbow_blood.pk3     .
+docker cp ${CID}:/usr/vol/freedm.wad            .
+docker cp ${CID}:/usr/vol/freedoom1.wad         .
+docker cp ${CID}:/usr/vol/freedoom2.wad         .
 
